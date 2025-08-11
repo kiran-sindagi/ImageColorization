94d9d4ed1425ec9f04c26b57620c257f8b8f7444
Kiran Sindagi <120112796+kiran-sindagi@users.noreply.github.com>
2025-08-11 13:33:19 +0530

    Changed route

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ def to_base64(img_array):
     byte_data = buf.getvalue()
     return base64.b64encode(byte_data).decode()
 
+MAX_DIMENSION = 1024 # Max width or height of 1024 pixels
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -63,6 +65,12 @@ def index():
             return render_template("index.html", error="No file uploaded")
 
         img = Image.open(file.stream).convert("RGB")
+        
+        # --- RESIZE THE IMAGE ---
+        # If the image is larger than our max dimension, shrink it down.
+        if max(img.size) > MAX_DIMENSION:
+            img.thumbnail((MAX_DIMENSION, MAX_DIMENSION))
+        
         img_np = np.array(img)
         colorized_img = colorizer(img_np)
 
@@ -74,5 +82,4 @@ def index():
             original_image=original_b64,
             colorized_image=colorized_b64
         )
-
     return render_template("index.html")
