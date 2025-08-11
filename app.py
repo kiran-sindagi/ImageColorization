7d1b9ed63229bf8e7413faee9631969fc4eaa8d7
Kiran Sindagi <120112796+kiran-sindagi@users.noreply.github.com>
2025-08-11 13:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,22 +21,31 @@ pts = pts.transpose().reshape(2, 313, 1, 1)
 net.getLayer(class8).blobs = [pts.astype("float32")]
 net.getLayer(conv8).blobs = [np.full([1, 313], 2.606, dtype="float32")]
 
+import gc # Import the garbage collector module at the top of your file
+
 def colorizer(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    # ... (initial code is the same)
     scaled = img.astype("float32") / 255.0
     lab = cv2.cvtColor(scaled, cv2.COLOR_RGB2LAB)
+    del scaled # Clean up the large float array
+
     resized = cv2.resize(lab, (224, 224))
     L = cv2.split(resized)[0]
     L -= 50
+
     net.setInput(cv2.dnn.blobFromImage(L))
     ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
     ab = cv2.resize(ab, (img.shape[1], img.shape[0]))
+
     L = cv2.split(lab)[0]
+    del lab # Clean up the second large array
+    
     colorized = np.concatenate((L[:, :, np.newaxis], ab), axis=2)
     colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2RGB)
     colorized = np.clip(colorized, 0, 1)
     colorized = (255 * colorized).astype("uint8")
+    
+    gc.collect() # Ask the garbage collector to run
     return colorized
 
 def to_base64(img_array):
